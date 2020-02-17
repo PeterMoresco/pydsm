98e8ef930891f321c716a1a1a416d772c046e002
PeterMoresco <pedro.moresco93@gmail.com>
2020-02-17 00:17:55 -0300

    Added a bash file to install the dependencies in Debain

---
+++ old/pythonInstall.sh
@@ -0,0 +1,19 @@
+# This script intalls some lybraries in a Windows computer that
+# are or intended to be used in this program and serves as
+# a reminder for later reference
+
+echo ================================================
+echo ============ Instalando pacotes python =========
+echo ================================================
+python3 -m pip install --upgrade pip
+python3 -m pip install numpy
+python3 -m pip install progressbar
+python3 -m pip install psutil
+python3 -m pip install pandas
+python3 -m pip install xlsxwriter
+python3 -m pip install memory_profiler
+python3 -m pip install pyfiglet
+python3 -m pip install colorama
+echo ================================================
+echo =================== Finalizado =================
+echo ================================================
